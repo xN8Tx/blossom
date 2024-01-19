9cf3047ebe7ebe2701510094d546aa96ecc9d6cf
Evgeniy Shteyn <theeldern8t@ya.ru>
2024-01-19 21:36:03 +0700

    [CLIENT] Some fixes

--- client/src/components/search-input/SearchInput.tsx
+++ client/src/components/search-input/SearchInput.tsx
@@ -34,7 +34,7 @@ export default function SearchInput({
       placeholder={placeholder}
       type='text'
       position='left'
-      color='first'
+      color='primary'
     >
       <SearchIcon />
     </ButtonInput>
--- client/src/ui/link/MyLink.tsx
+++ client/src/ui/link/MyLink.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 
-import type { LinkProps } from '../../models/uiProps';
-
 import style from './MyLink.module.scss';
+
+type LinkProps = {
+  children: React.ReactNode;
+  to: string;
+};
 
 export default function MyLink({ children, to }: LinkProps) {
   return (
